3953c4adca1f842087da394a5d85561ce9c28e67
skyg547 <skyg547@gmail.com>
2023-11-01 23:32:33 +0900

    feat: make battery class and interface
    to implement di
    
    #issueX

---
+++ spring4-project/src/main/java/com/ho/Battery.java
@@ -0,0 +1,6 @@
+package com.ho;
+
+public interface Battery {
+
+	int getBatteryValue();
+}
---
+++ spring4-project/src/main/java/com/ho/NoramlBattery.java
@@ -0,0 +1,12 @@
+package com.ho;
+
+public class NoramlBattery implements Battery {
+	NoramlBattery() {
+
+	}
+
+	@Override
+	public int getBatteryValue() {
+		return 0;
+	}
+}
