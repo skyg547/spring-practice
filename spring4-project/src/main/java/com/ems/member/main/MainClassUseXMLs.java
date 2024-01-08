1312dbc82f9702482df702acf0904d2159e73faa
skyg547 <skyg547@gmail.com>
2024-01-09 00:41:57 +0900

    refactor: code reformatting
    To improve readability
    
    #issueX

--- spring4-project/src/main/java/com/ems/member/main/MainClassUseXMLs.java
+++ spring4-project/src/main/java/com/ems/member/main/MainClassUseXMLs.java
@@ -13,30 +13,21 @@ public class MainClassUseXMLs {
 
 	public static void main(String[] args) {
 
-		String[] sNums = {"H39r8djakndfae32", "H39asdfaelu42o23", "H39iiemamca8w9h4",
-				"H39lkmn754fghia7", "H39plo865cuy8k92", "H39mnbviiaed89q1",
-				"H399omjjyv56t3d5", "H39lczaqwg644gj8", "H39ymbcsh74thgh2",
-				"H39lesvj7544vf89"};
+		String[] sNums = {"H39r8djakndfae32", "H39asdfaelu42o23", "H39iiemamca8w9h4", "H39lkmn754fghia7", "H39plo865cuy8k92", "H39mnbviiaed89q1", "H399omjjyv56t3d5", "H39lczaqwg644gj8", "H39ymbcsh74thgh2", "H39lesvj7544vf89"};
 
-		String[] sIds = {"rabbit", "hippo", "raccoon", "elephant", "lion",
-				"tiger", "pig", "horse", "bird", "deer"};
+		String[] sIds = {"rabbit", "hippo", "raccoon", "elephant", "lion", "tiger", "pig", "horse", "bird", "deer"};
 
-		String[] sPws = {"96539", "94875", "15284", "48765", "28661",
-				"60915", "30028", "29801", "28645", "28465"};
+		String[] sPws = {"96539", "94875", "15284", "48765", "28661", "60915", "30028", "29801", "28645", "28465"};
 
-		String[] sNames = {"agatha", "barbara", "chris", "doris", "elva",
-				"fiona", "holly", "jasmin", "lena", "melissa"};
+		String[] sNames = {"agatha", "barbara", "chris", "doris", "elva", "fiona", "holly", "jasmin", "lena", "melissa"};
 
 		int[] sAges = {19, 22, 20, 27, 19, 21, 19, 25, 22, 24};
 		String[] sGenders = {"M", "W", "W", "M", "M", "M", "W", "M", "W", "W"};
-		String[] sMajors = {"English Literature", "Korean Language and Literature",
-				"French Language and Literature", "Philosophy", "History",
-				"Law", "Statistics", "Computer", "Economics", "Public Administration"};
+		String[] sMajors = {"English Literature", "Korean Language and Literature", "French Language and Literature", "Philosophy", "History", "Law", "Statistics", "Computer", "Economics", "Public Administration"};
 
 //		StudentAssembler assembler = new StudentAssembler();
 		String[] appCtxs = {"classpath:appCtx1.xml", "classpath:appCtx2.xml", "classpath:appCtx3.xml"};
-		GenericXmlApplicationContext ctx =
-				new GenericXmlApplicationContext(appCtxs);
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(appCtxs);
 
 		EMSInformationService informationService = ctx.getBean("informationService", EMSInformationService.class);
 		informationService.outputEMSInformation();
@@ -44,15 +35,13 @@ public class MainClassUseXMLs {
 //		StudentRegisterService registerService = assembler.getRegisterService();
 		StudentRegisterService registerService = ctx.getBean("registerService", StudentRegisterService.class);
 		for (int j = 0; j < sNums.length; j++) {
-			Student student = new Student(sNums[j], sIds[j], sPws[j], sNames[j],
-					sAges[j], sGenders[j], sMajors[j]);
+			Student student = new Student(sNums[j], sIds[j], sPws[j], sNames[j], sAges[j], sGenders[j], sMajors[j]);
 			registerService.register(student);
 		}
 
 //		StudentModifyService modifyService = assembler.getModifyService();
 		StudentModifyService modifyService = ctx.getBean("modifyService", StudentModifyService.class);
-		modifyService.modify(new Student("H39lesvj7544vf89", "deer", "00000", "melissa",
-				26, "W", "Vocal Music"));
+		modifyService.modify(new Student("H39lesvj7544vf89", "deer", "00000", "melissa", 26, "W", "Vocal Music"));
 
 //		StudentSelectService selectService = assembler.getSelectService();
 		StudentSelectService selectService = ctx.getBean("selectService", StudentSelectService.class);
@@ -88,8 +77,7 @@ public class MainClassUseXMLs {
 			Scanner scanner = new Scanner(System.in);
 			String str = "";
 
-			System.out.println("\n==================================================================="
-					+ "==============================================================================");
+			System.out.println("\n===================================================================" + "==============================================================================");
 			System.out.println("Select number.");
 			System.out.println("1. Check student information");
 			System.out.println("2. Exit");
