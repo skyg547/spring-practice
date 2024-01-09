d087d33084372f313e48b5f246869c8773d41961
skyg547 <skyg547@gmail.com>
2024-01-10 00:48:14 +0900

    feat: initial WordRegisterService
    regist wordset through worddao
    
    #issueX

---
+++ spring4-project/src/main/java/com/word/service/WordRegisterService.java
@@ -0,0 +1,30 @@
+package com.word.service;
+
+import com.word.WordSet;
+import com.word.dao.WordDao;
+
+public class WordRegisterService {
+	private WordDao wordDao;
+
+	public WordRegisterService(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+
+	public void register(WordSet wordSet) {
+		String wordKey = wordSet.getWordKey();
+		if (verify(wordKey)) {
+			wordDao.insert(wordSet);
+		} else {
+			System.out.println("The word has already registered");
+		}
+	}
+
+	boolean verify(String wordKey) {
+		WordSet wordSet = wordDao.select(wordKey);
+		return wordSet == null;
+	}
+
+	public void setWordDao(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+}
