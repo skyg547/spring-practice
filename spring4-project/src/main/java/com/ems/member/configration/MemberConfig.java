56eae2610d576aafc38d916106997322c50b5f04
skyg547 <skyg547@gmail.com>
2024-01-23 23:42:32 +0900

    feat: create MemberConfig
    make context with java
    
    #issueX

---
+++ spring4-project/src/main/java/com/ems/member/configration/MemberConfig.java
@@ -0,0 +1,16 @@
+package com.ems.member.configration;
+
+import com.ems.member.DataBaseConnectionInfo;
+import com.ems.member.dao.StudentDao;
+import com.ems.member.service.*;
+import com.sun.media.sound.AbstractMidiDeviceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.*;
+
+@Configuration
+public class MemberConfig {
+
+}
